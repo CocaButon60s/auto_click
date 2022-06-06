335a320c5c75b77d736ed7631604f6bca10f98f2
es-ikuno.hiroyuki <ikuno.hiroyuki@jp.panasonic.com>
2022-06-06 19:26:51 +0900

    コミット

--- auto_click.py
+++ auto_click.py
@@ -1,4 +1,5 @@
 import pyautogui as pg
+import sys, time
 
 def print_pos():
     """マウスのポジョン出力する
@@ -9,12 +10,19 @@ def print_pos():
         x, y = pg.position()
         print(str(x) + "," + str(y))
 
+def test():
+    i = 0
+    while True:
+        print(str(i))
+        i += 1
+        time.sleep(1)
 
 def main():
-    pg.PAUSE = 1
+    pg.PAUSE = 60
     pg.FAILSAFE = True
     while True: pg.click(1032, 543)
 
 if __name__ == "__main__":
     try: main()
-    except: print("終了")
+    except Exception as e: print(e)
+    print("END")
